cbb15d44b49ae12403201010d7d5322e4e6973ce
Jason Li <jasonli@jason-li.local>
2024-07-15 10:28:29 -0700

    project setup

---
+++ server/cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		log.Fatalln("usage: server [IP_ADDR]")
+	}
+
+	addr := args[0]
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v\n", err)
+	}
+
+	defer func(lis net.Listener) {
+		if err := lis.Close(); err != nil {
+			log.Fatalf("unexpected error: %v", err)
+		}
+	}(lis)
+
+	var opts []grpc.ServerOption
+	s := grpc.NewServer(opts...)
+
+	log.Printf("listening at %s\n", addr)
+
+	defer s.Stop()
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v\n", err)
+	}
+}
